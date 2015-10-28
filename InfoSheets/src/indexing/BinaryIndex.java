09ecebb9fb6e9434d1c114fe9010163ea6e1ee1a
Soeren <loizuf.sn@gmail.com>
2015-10-28 17:25:48 +0100

    added binary-Index class. No functionality yet.

---
+++ InfoSheets/src/indexing/BinaryIndex.java
@@ -0,0 +1,15 @@
+package indexing;
+
+import java.util.HashSet;
+
+public class BinaryIndex {
+
+	private HashSet<String> allWordsinCollection;
+	
+	public BinaryIndex() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	// allWordsinCollection.addAll(document.getUniqueWordList());
+
+}
