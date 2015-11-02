66aeebe680c43318384de23b6f60df6db268d861
Soeren <loizuf.sn@gmail.com>
2015-11-02 13:43:49 +0100

    mainly refactoring and renaming. added catch for the case that the search doesnt return a result.

--- InfoSheets/src/indexing/InvertedIndex.java
+++ InfoSheets/src/indexing/InvertedIndex.java
@@ -2,10 +2,8 @@ package indexing;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
 
-public class InvertedIndex extends AbstractIndex {
+public class InvertedIndex extends AbstractIndex implements Index {
 
 	/*
 	 * This Hashmap represents the Term-Document Matrix. The outer Key is the
@@ -23,28 +21,35 @@ public class InvertedIndex extends AbstractIndex {
 	public InvertedIndex(ArrayList<Document> collection) {
 		invertedIndex = new HashMap<>();
 		documentNameList = new HashMap<>();
-		generateWordsInCollectionList(collection);
+		goThroughCollection(collection);
 	}
 
 	// this is a testmethod. The real search should of course use the Query-class
-	public ArrayList<String> searchForSingleWord(String word) {
-		ArrayList<String> searchResult  = new ArrayList<>();
-		for (int docNumber : invertedIndex.get(word)) {
-			searchResult.add(documentNameList.get(docNumber));
+	public ArrayList<Integer> searchForSingleWord(String word) {
+		ArrayList<Integer> result = invertedIndex.get(word);
+		if(result==null){
+			return new ArrayList<Integer>();
 		}
-		return searchResult;
+		return result;
+	}
+	
+	public String getDocumentName(int number) {
+		String name;
+		name = documentNameList.get(number);
+		return name;
 	}
 
-	private void generateWordsInCollectionList(ArrayList<Document> collection) {
+	private void goThroughCollection(ArrayList<Document> collection) {
 		int counter = 0;
 		for (Document document : collection) {
 			documentNameList.put(counter, document.getName());
-			generateTermDocumentMatrix(document.getUniqueWordList(), counter);
+			generateInvertedIndex(document.getUniqueWordList(), counter);
 			counter++;
 		}
+		// No further sorting necessary because the inverted Index is already in ascending order
 	}
 
-	private void generateTermDocumentMatrix(ArrayList<String> wordsInDocument, int counter) {
+	private void generateInvertedIndex(ArrayList<String> wordsInDocument, int counter) {
 		for (String word : wordsInDocument) {
 			if (invertedIndex.containsKey(word)) {
 				invertedIndex.get(word).add(counter);
