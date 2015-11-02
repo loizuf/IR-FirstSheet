80ee349ddaca578c79b7be9d5b0acdc015fe5104
Soeren <loizuf.sn@gmail.com>
2015-11-02 13:47:09 +0100

    Eliminated the filereader reference, file is now completely read in maincontroller and as one string instead of single tokens, tokenizing is performed completely in the tokenizer. added interaction with the user, binary search (AND) is possible, single word search also.

--- InfoSheets/src/controller/MainController.java
+++ InfoSheets/src/controller/MainController.java
@@ -1,68 +1,108 @@
 package controller;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Scanner;
 
-import indexing.InvertedIndex;
 import indexing.Document;
-import indexing.FileReader;
+import indexing.InvertedIndex;
+import indexing.Query;
+import searchengines.BooleanSearchEngine;
 
 public class MainController {
 
 	/* Variables */
-	private static FileReader fileReader;
 	private static ArrayList<Document> collection;
 	private static InvertedIndex invertedIndex;
 
 	/* Constants */
 	private static final String LoremIpsumCollectionDirectoryPath = "collections";
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws IOException {
 		instantiate();
 		readCollection();
 		createIndex();
-		// only for test purposes
-		test();
+		Query query = getQuery();
+		ArrayList<String> mergeResult = performBooleanSearch(query);
+		postResults(mergeResult, query);
+	}
+
+	private static void postResults(ArrayList<String> mergeResult, Query query) {
+		ArrayList<String> queryWordList = query.getUniqueWordList();
+		System.out.println("+++++++");
+		System.out.println("");
+		switch (queryWordList.size()){
+		case 0:
+			System.out.println("No query recognized.");
+			break;
+		case 1:
+			if(mergeResult.size()>0){
+				System.out.println("The word '"+queryWordList.get(0)+"' can be found in the documents:");
+			} else {
+				System.out.println("No match for the word '"+queryWordList.get(0)+"'");
+			}
+			break;
+		default:
+			if(mergeResult.size()>0){
+				System.out.println("The words '"+queryWordList.get(0)+"' and '"+queryWordList.get(1)+"' can be found in the documents:");
+			} else {
+				System.out.println("No match for the words '"+queryWordList.get(0)+"' and '"+queryWordList.get(1)+"'");
+			}
+			break;
+		}
+		for (String string : mergeResult) {
+			System.out.println(string);
+		}
+	}
+
+	private static ArrayList<String> performBooleanSearch(Query query) {
+		ArrayList<Integer> resultPostingsList = BooleanSearchEngine.performSearch(invertedIndex, query);
+		
+		ArrayList<String> searchResult  = new ArrayList<>();
+		for (int docNumber : resultPostingsList) {
+			searchResult.add(invertedIndex.getDocumentName(docNumber));
+		}
+		return searchResult;
+	}
+
+	private static Query getQuery() throws IOException {
+		System.out.println("Please enter your query:");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String input = reader.readLine();
+		System.out.println("Boolean Retrieval? y/n");
+		String testForRetrieval = reader.readLine().toLowerCase();
+		boolean booleanRetrieval = true;
+		if(testForRetrieval.equals("n")){
+			booleanRetrieval = false;
+		}
+		Query currentQuery = new Query(input, booleanRetrieval);
+		return currentQuery;
 	}
 
 	private static void createIndex() {
 		invertedIndex = new InvertedIndex(collection);
 	}
 
-	private static void test() {
-		System.out.println(collection.size());
-		
-		Document test = collection.get(0);
-		System.out.println(test.getName());
-		
-		ArrayList<String> uniqueWords = test.getUniqueWordList();
-		System.out.println(uniqueWords.size());
-		
-		ArrayList<String> allWords = test.getWordList();
-		System.out.println(allWords.size());
-		
-		ArrayList<String> searchResult = invertedIndex.searchForSingleWord("Lorem");
-		System.out.println(searchResult);
-	}
-
 	private static void instantiate() throws FileNotFoundException {
 		// Instantiate fields
-		fileReader = new FileReader();
 		collection = new ArrayList<>();
 	}
 
 	/*
-	 * directory is the overall document location. this method iterates over all documents in directory.
-	 * for each file in directory we create a document object with relevant information and store
-	 * that in collection.
+	 * directory is the overall document location. this method iterates over all
+	 * documents in directory. for each file in directory we create a document
+	 * object with relevant information and store that in collection.
 	 */
 	private static void readCollection() throws FileNotFoundException {
 		File directory = new File(LoremIpsumCollectionDirectoryPath);
 		File[] directoryListing = directory.listFiles();
 		if (directoryListing != null) {
 			for (File child : directoryListing) {
-				Document document = fileReader.read(child);
+				Document document = readDocument(child);
 				collection.add(document);
 			}
 		} else {
@@ -71,5 +111,18 @@ public class MainController {
 			// to avoid race conditions with another process that deletes
 			// directories.
 		}
+	}
+
+	public static Document readDocument(File file) throws FileNotFoundException {
+		/*
+		 * Scanner uses Whitespace as delimiter. Documents and query should use
+		 * the Tokenizer class to change that
+		 */
+		Scanner scanner = new Scanner(file).useDelimiter("\\A");
+		String entireTextFile = scanner.next();
+		scanner.close();
+
+		Document document = new Document(entireTextFile, file.getName());
+		return document;
 	}
 }
