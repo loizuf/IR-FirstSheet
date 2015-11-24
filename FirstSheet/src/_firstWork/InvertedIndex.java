daca1ab3a18f305fd61b5ee51d54eb974ff5a5a6
davidelsweiler <davidelsweiler@pc101140754x.uni-regensburg.de>
2015-11-24 12:59:29 +0100

    David has completed the worksheet

--- FirstSheet/src/_firstWork/InvertedIndex.java
+++ FirstSheet/src/_firstWork/InvertedIndex.java
@@ -2,6 +2,7 @@ package _firstWork;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 
@@ -11,23 +12,83 @@ import _firstGiven.BooleanIndexable;
 public class InvertedIndex {
 
 	private HashMap<String, ArrayList<Integer>> invertedIndex;
-
-
+	
+	
+	
 	// Bearbeiten sie Aufgabe 1 hier.
 	public InvertedIndex(ArrayList<BooleanDocument> collection) {
 		
+		invertedIndex = new HashMap<String,ArrayList<Integer>>();
+		
+		Iterator<BooleanDocument> it = collection.iterator();
+		int docId = -1;
+		while (it.hasNext()){
+			BooleanDocument doc = it.next();
+			docId++;
+			System.out.println("adding doc" + docId);
+			
+			// we will only take unique terms just now as this is a boolean model
+			ArrayList<String> terms = doc.getUniqueWordList();
+			Iterator<String> termIt = terms.iterator();
+			while (termIt.hasNext()) {
+				String term = termIt.next();
+				indexTerm(term,docId);
+			}
+			
+		}
+		
+		System.out.println("The index has "+ invertedIndex.keySet().size() +"terms");
 	}
 
+	// Second method required for Aufgabe 1; index term
 
+	public void indexTerm(String t, int doc){
+		
+		ArrayList<Integer> docList = invertedIndex.get(t);
+		
+		if (docList==null)
+			invertedIndex.put(t, new ArrayList<Integer>());
+		else {
+			docList.add(doc);
+		}
+		
+	}
+	
 	// Bearbeiten sie Aufgabe 2 hier.
 	public  ArrayList<Integer> searchForSingleWord(String word) {
-		return null;
+		return invertedIndex.get(word);
 	}
 	
 	// Bearbeiten sie Aufgabe 4 hier
 	public ArrayList<Integer> performANDMerge(String firstWord, String secondWord) {
-		return null;
+		//empty arraylist to place results
+		ArrayList<Integer> results = new ArrayList<Integer>();
+		
+		//get postingslist and an iterator for firstWord
+		Iterator<Integer>postingsX = searchForSingleWord(firstWord).iterator();
+		//get postingslist and an iterator for secondWord
+		Iterator<Integer>postingsY = searchForSingleWord(secondWord).iterator();
+		
+		int x = postingsX.next();
+		int y = postingsY.next();
+		
+		while (postingsX.hasNext() || postingsY.hasNext()){
+			if (x == y) {
+				results.add(x);
+				if (postingsX.hasNext()){x = postingsX.next();}
+				if (postingsY.hasNext()){y = postingsY.next();}
+			}
+			else if (x < y){
+				if (postingsX.hasNext()){x = postingsX.next();}
+			}
+			else{
+				if (postingsY.hasNext()){y = postingsY.next();}
+			}
+		}
+		
+		return results;
+		
 	}
 	
-	// Für Aufgabe 5 erweitern sie diese Klasse um weitere Methoden
+	// Fï¿½r Aufgabe 5 erweitern sie diese Klasse um weitere Methoden
 }
--- FirstSheet/src/_firstWork/MainController.java
+++ FirstSheet/src/_firstWork/MainController.java
@@ -2,6 +2,7 @@ package _firstWork;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import _firstGiven.BooleanDocument;
 import _firstGiven.FileReader;
@@ -13,17 +14,29 @@ public class MainController {
 		// collection - Liste aller Document-Objekte
 		ArrayList<BooleanDocument> collection = FileReader.readCollection();
 
-		// invertedIndex - Repräsentation des InvertedIndex
+		// invertedIndex - Reprï¿½sentation des InvertedIndex
 		InvertedIndex invertedIndex = new InvertedIndex(collection);
 
-		String firstTerm = null;
-		String secondTerm = null;
+		// let's get a query from the user via the command line
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter a query:");
+		String query = scanner.nextLine();
+		//String query = "david AND theo";
+		
+		// test for aufgabe 2
+		//System.out.println(invertedIndex.searchForSingleWord(query));
+		
+		//breakdown the query into two terms
+		String[] qterms = query.trim().split("AND");
+		//for (String q:qterms)
+		//	System.out.println(q.trim().toLowerCase());
+		
 
 		// Diese Methode liest einen String vom User ein und splittet diesen in 2 Strings
-		getQueryTerms(firstTerm, secondTerm);
+		//getQueryTerms(firstTerm, secondTerm);
 
 		// mergeResult- Liste aller IDs der Dokumente die der Query entsprechen
-		ArrayList<Integer> mergeResult = invertedIndex.performANDMerge(firstTerm, secondTerm);
+		ArrayList<Integer> mergeResult = invertedIndex.performANDMerge(qterms[0], qterms[1]);
 
 		postResults(mergeResult);
 	}
